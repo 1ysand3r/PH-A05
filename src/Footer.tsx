54428b63f5b88942e4fd1fa688af0c03d0637dde
1ysand3r <rashedrafi145@gmail.com>
2026-09-14 01:51:45 +0600

    Fixed more issues

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
     <footer className="grid grid-cols-5 p-8">
       <div className="col-span-2 gap-4">
         <img src={Logo} alt="" />
-        <p>
+        <p className="font-thin text-sm text-mist-700 py-2">
           Curated tools, technologies, and resources for developers building
           modern software.
         </p>
@@ -13,10 +13,11 @@ const Footer = () => {
           <a href="">Twitter</a>
           <a href="">LinkedIn</a>
         </div>
+        <p className="text-sm text-mist-400 py-9">© 2026 Dev Stack. All rights reserved.</p>
       </div>
       <div className="gap-4">
+        <h3 className="font-bold">PRODUCT</h3>
         <ul>
-          <h3 className="font-bold">PRODUCT</h3>
           <li>Home</li>
           <li>Technologies</li>
           <li>Projects</li>
@@ -24,14 +25,18 @@ const Footer = () => {
       </div>
       <div className="gap-4">
         <h3 className="font-bold">COMPANY</h3>
-        <li>About</li>
-        <li>Contact</li>
-        <li>Careers</li>
+        <ul>
+          <li>About</li>
+          <li>Contact</li>
+          <li>Careers</li>
+        </ul>
       </div>
       <div className="gap-4">
         <h3 className="font-bold">LEGAL</h3>
-        <li>Privacy Policy</li>
-        <li>Terms of Service</li>
+        <ul>
+          <li>Privacy Policy</li>
+          <li>Terms of Service</li>
+        </ul>
       </div>
     </footer>
   );

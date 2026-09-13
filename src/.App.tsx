e233a0656f3573f416eb30e0f519a5d8fcc8776e
1ysand3r <rashedrafi145@gmail.com>
2026-09-13 23:22:27 +0600

    Initialized with Vite, TailwindCSS & DaisyUI

---
+++ src/.App.tsx
@@ -0,0 +1,17 @@
+import Banner from "./Banner.tsx"
+import Footer from "./Footer.tsx"
+import Header from "./Header.tsx"
+
+function App() {
+
+  return (
+    <>
+      {/* <h1 className="text-9xl text-red-500 italic">Hello</h1> */}
+      <Header/>
+      <Banner/>
+      <Footer/>
+    </>
+  )
+}
+
+export default App
